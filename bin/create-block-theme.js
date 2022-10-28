474178dbb39724052111d0e98baa08fea52e6c4a
Alfredo Navas <elpuas@gmail.com>
2022-10-28 17:08:00 -0600

    add chalk

--- bin/create-block-theme.js
+++ bin/create-block-theme.js
@@ -2,40 +2,41 @@
 const runCommand         = require('./lib/runCommand.js');
 const updateJson         = require('./lib/updateJson.js');
 const themeFileReader    = require('./lib/themeFileReader.js');
+const chalk              = require('chalk');
 const repoName           = process.argv[2];
 const gitCheckoutCommand = `git clone https://github.com/elpuas/create-block-based-theme.git ${repoName}`;
 const npmInstallCommand  = `cd ${repoName} && npm install`;
 let checkedOut
-const format = /^[0-9-]+$/gm;
+const format  = /^[0-9-]+$/gm;
 
-if (typeof repoName !== 'undefined' && format.test(repoName) ) {
+if (typeof repoName !== 'undefined' && ! format.test(repoName) ) {
     checkedOut = runCommand(gitCheckoutCommand);
 } else {
-    console.error('Error: Please use a valid name for your theme');
+    console.error(`${chalk.red('Error:')} Please use a valid name for your theme`);
     console.log('Script Example: npx @elpuas/create-block-theme my-super-awesome-theme');
     process.exit(1);
 }
 
-console.log(`Creating a new WordPress theme called ${repoName}...`);
+console.log(`Creating a new ${chalk.green('WordPress')} theme called ${repoName}...`);
 console.log('');
 console.log('Cloning the repo...');
 
 if (!checkedOut) {
-    console.error('Failed to checkout the repo');
+    console.error(chalk.red('Failed to checkout the repo'));
     process.exit(1);
 }
 
-console.log('Repo cloned successfully.');
+console.log(`Repo cloned ${chalk.green('successfully')}!`);
 console.log('');
-console.log(`Installing dependencies for ${repoName}...`);
+console.log(`Installing dependencies for ${chalk.green(repoName)}...`);
 
 const installed = runCommand(npmInstallCommand);
 if (!installed) {
-    console.error('Failed to install dependencies');
+    console.error(chalk.error('Failed to install dependencies'));
     process.exit(1);
 }
 
-console.log('Dependencies installed successfully.');
+console.log(`Dependencies installed ${chalk.green('successfully')}.`);
 console.log('');
 console.log('Updating package.json...');
 console.log('');
@@ -47,9 +48,10 @@ console.log('');
 
 themeFileReader( repoName, 'style.css' );
 
-console.log(`Success! Created ${repoName} at ${process.cwd()}/${repoName}`);
+console.log(`Success! Created ${chalk.green(repoName)} at ${process.cwd()}/${repoName}`);
 console.log('');
-console.log(`cd ${repoName} and start hacking.`);
+console.log(`cd ${chalk.green(repoName)} and start hacking.`);
+console.log('');
 console.log('Inside that directory, you can run several commands:');
 console.log('');
 console.log('  npm run start');
@@ -58,5 +60,7 @@ console.log('');
 console.log('  npm run build');
 console.log('    Bundles the app files for production.');
 console.log('');
-console.log('Happy Coding!');
+console.log('*******************');
+console.log(`*  ${chalk.green('Happy Coding!')}  *`);
+console.log('*******************');
 console.log('');
